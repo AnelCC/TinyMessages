54b84a9bd34c9309ecb55f1df03dcc69e96bf4cb
Anel CC <anel.calvo.cervantes@gmail.com>
2020-05-03 18:11:06 -0400

    [Anel][COM_00001_Toast] 3. Created a custom toast: Short and Long

--- app/src/main/java/com/anelcc/usercomunacation/ToastActivity.kt
+++ app/src/main/java/com/anelcc/usercomunacation/ToastActivity.kt
@@ -3,8 +3,10 @@ package com.anelcc.usercomunacation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.RadioButton
+import android.widget.TextView
 import android.widget.Toast
 
 class ToastActivity : AppCompatActivity(), View.OnClickListener {
@@ -21,6 +23,7 @@ class ToastActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v!!.id) {
             R.id.btnShowToast -> showToast()
+            R.id.btnShowCustomToast -> showCustomToast()
         }
     }
     private fun showToast() {
@@ -38,6 +41,33 @@ class ToastActivity : AppCompatActivity(), View.OnClickListener {
         // Create and show the toast message
         val toast = Toast.makeText(this, "This is a toast $toastType", toastDuration)
         toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
+    }
+
+    private fun showCustomToast() {
+        var toastType = "";
+        val toastDuration: Int
+        val rbShort = findViewById<View>(R.id.rbShort) as RadioButton
+
+        if (rbShort.isChecked) {
+            toastType = "Short"
+            toastDuration = Toast.LENGTH_SHORT
+        } else {
+            toastType = "Long"
+            toastDuration = Toast.LENGTH_LONG
+        }
+
+        // Get the custom layout and inflate it
+        val inflater = LayoutInflater.from(this)
+        val layout = inflater.inflate(R.layout.custom_toast_layout, findViewById(R.id.customToastLayout))
+
+        // Build a toast message that uses the custom layout
+        val tv = layout.findViewById(R.id.textContent) as TextView
+        tv.text = "This is a custom toast $toastType"
+        val toast = Toast(applicationContext)
+        toast.duration = toastDuration
+        toast.setGravity(Gravity.BOTTOM or Gravity.START, 100, 100)
+        toast.view = layout
         toast.show()
     }
 }
