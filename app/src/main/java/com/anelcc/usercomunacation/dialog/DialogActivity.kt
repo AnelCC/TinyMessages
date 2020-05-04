d49c985fc4734deee1d4031bd2bc9c5379e5a77f
Anel CC <anel.calvo.cervantes@gmail.com>
2020-05-04 01:39:49 -0400

    [Anel][COM_00003_Dialogs] 1. Created DatePickerDialog

--- app/src/main/java/com/anelcc/usercomunacation/dialog/DialogActivity.kt
+++ app/src/main/java/com/anelcc/usercomunacation/dialog/DialogActivity.kt
@@ -1,5 +1,6 @@
 package com.anelcc.usercomunacation.dialog
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,16 +9,8 @@ import androidx.fragment.app.DialogFragment
 import com.anelcc.usercomunacation.R
 import com.anelcc.usercomunacation.dialog.simpledialog.SimpleDialogFragment
 import com.anelcc.usercomunacation.dialog.simpledialog.SimpleDialogListener
+import java.util.*
 
-/*
-A dialog is a small window that prompts the user to make a decision or enter additional information.
-A dialog does not fill the screen and is normally used for modal events that require users to
-take an action before they can proceed.
-
-- Interumptive - stop current program flow
-- Require the user to take action
-- Maintain the user's current context
-*/
 class DialogActivity : AppCompatActivity(), View.OnClickListener,
     SimpleDialogListener {
     private val TAG = "DialogActivity"
@@ -27,12 +20,15 @@ class DialogActivity : AppCompatActivity(), View.OnClickListener,
         setContentView(R.layout.activity_dialog)
 
         findViewById<View>(R.id.btnSimpleDialog).setOnClickListener(this)
+        findViewById<View>(R.id.btnShowDatePicker).setOnClickListener(this)
 
     }
 
-    override fun onClick(v: View?) {
-        showSimpleDialog()
-
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnSimpleDialog -> showSimpleDialog()
+            R.id.btnShowDatePicker ->  showCalendar()
+        }
     }
 
     private fun showSimpleDialog() {
@@ -52,5 +48,18 @@ class DialogActivity : AppCompatActivity(), View.OnClickListener,
 
     override fun onNeutralResult(dlg: DialogFragment) {
         Log.i(TAG, "onNeutralResult")
+    }
+
+    private fun showCalendar() {
+        // Get a calendar instance
+        val cal = Calendar.getInstance()
+        // Create a DatePickerDialog
+        val datePicker = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                Log.i(TAG, String.format("Date Chosen -- day: %d, month: %d, year: %d", dayOfMonth, monthOfYear, year)) },
+            cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
+        // Set the title and show the dialog
+        datePicker.setTitle("Choose a Date")
+        datePicker.show()
     }
 }
