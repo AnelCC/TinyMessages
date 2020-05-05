c0c15e88d4ff0fb6c3094241e4447e0412113a27
Anel CC <anel.calvo.cervantes@gmail.com>
2020-05-05 09:27:31 -0400

    [Anel][COM_00004_Notifications] 2. add simple notification with small icon, text and large icon

--- app/src/main/java/com/anelcc/usercomunacation/notification/NotificationActivity.kt
+++ app/src/main/java/com/anelcc/usercomunacation/notification/NotificationActivity.kt
@@ -1,25 +1,44 @@
 package com.anelcc.usercomunacation.notification
 
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
 import com.anelcc.usercomunacation.R
 
-/*
-A notification is a message that Android displays outside your app's UI
-to provide the user with reminders, communication from other people,
-or other timely information from your app. Users can tap the notification
-to open your app or take an action directly from the notification.
-
-- Displays outside of you normal app user interface.
-- Trigger specific parts of your application when tapped.
-- Take various forms depending on their information.
-- Can have individual action item associated.
-- Can appear on the user's lock screen.
-*/
-class NotificationActivity : AppCompatActivity() {
+class NotificationActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+        private val NOTIFY_ID = 1001
+        private val NOTIFY_CHANNEL = "MY_CHANNEL"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+        findViewById<View>(R.id.btnNotification).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        createNotification()
+    }
+
+    private fun createNotification() {
+        val builder = NotificationCompat.Builder(this, NOTIFY_CHANNEL)
+        // Set the three required items all notifications must have
+        builder.setSmallIcon(R.drawable.ic_notifications_accent)
+        builder.setContentTitle("Sample Notification")
+        builder.setContentText("This is a sample notification")
+        // Set the notification to cancel when the user taps on it
+        builder.setAutoCancel(true)
+        // Set the large icon to be our app's launcher icon
+        builder.setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.user_woman_icon))
+
+        // Build the finished notification and then display it to the user
+        val notification = builder.build()
+        val mgr = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        mgr.notify(NOTIFY_ID, notification)
     }
 }
